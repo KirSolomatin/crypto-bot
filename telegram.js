a9f4863f1cdf8bfcbf9ac983aa844d27d5cab15a
Yaganych <babayagakiril@gmail.com>
2026-09-13 19:52:05 +0200

    feat(eth&gram): add eth and gram prices to start command and hourly updates

--- telegram.js
+++ telegram.js
@@ -7,9 +7,18 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 async function getBitcoinPrice () {
     const bitcoin = await axios.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"); 
-    return Number(bitcoin.data.price).toFixed(2);
+    return Number(bitcoin.data.price).toFixed(3);
 }
 
+async function getEthereumPrice () {
+    const ethereum = await axios.get("https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT");
+    return Number(ethereum.data.price).toFixed(3);
+}
+
+async function getGramPrice () {
+    const gram = await axios.get("https://api.binance.com/api/v3/ticker/price?symbol=GRAMUSDT");
+    return Number(gram.data.price).toFixed(3); 
+}
 async function startBot() {
     let chatId  = null;
 
@@ -17,13 +26,19 @@ async function startBot() {
     bot.onText(/\/start/, async (msg) => {
         chatId = msg.chat.id;
         const bitcoinPrice = await getBitcoinPrice();
-        bot.sendMessage(chatId, `Current Bitcoin price: ${bitcoinPrice}$`)
+        const ethereumPrice = await getEthereumPrice();
+        const gramPrice = await getGramPrice();
+
+        bot.sendMessage(chatId, `Current Bitcoin price: ${bitcoinPrice}$ \nCurrent Ethereum price: ${ethereumPrice}$ \nCurrent Gram price: ${gramPrice}$`)
     });
 
     setInterval(async () => {
         if (!chatId) return;
         const bitcoinPrice = await getBitcoinPrice();
-        bot.sendMessage(chatId, `Current Bitcoin price: ${bitcoinPrice}$`)}, 3600000);
+        const ethereumPrice = await getEthereumPrice();
+        const gramPrice = await getGramPrice();
+
+        bot.sendMessage(chatId, `Current Bitcoin price: ${bitcoinPrice}$ \n Current Ethereum price: ${ethereumPrice}$ \n Current Gram price: ${gramPrice}$`)}, 3600000);
 }
 
 module.exports = startBot;
