8d4825aa952421714e3164bd6fd26d71a7c0b4bc
Yaganych <babayagakiril@gmail.com>
2026-09-13 19:33:01 +0200

    feat(bitcoin): add /start command with BTC price and hourly updates

--- telegram.js
+++ telegram.js
@@ -1,12 +1,29 @@
 require('dotenv').config();
+
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
-function startBot() {
-    bot.on("message", (msg) => {
-        bot.sendMessage(msg.chat.id, "Hello, World!")
+async function getBitcoinPrice () {
+    const bitcoin = await axios.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"); 
+    return Number(bitcoin.data.price).toFixed(2);
+}
+
+async function startBot() {
+    let chatId  = null;
+
+    
+    bot.onText(/\/start/, async (msg) => {
+        chatId = msg.chat.id;
+        const bitcoinPrice = await getBitcoinPrice();
+        bot.sendMessage(chatId, `Current Bitcoin price: ${bitcoinPrice}$`)
     });
+
+    setInterval(async () => {
+        if (!chatId) return;
+        const bitcoinPrice = await getBitcoinPrice();
+        bot.sendMessage(chatId, `Current Bitcoin price: ${bitcoinPrice}$`)}, 3600000);
 }
 
 module.exports = startBot;
